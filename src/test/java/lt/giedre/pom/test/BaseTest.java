46baf9eec362e8cb9712fe31132a9008350f8c5a
giedreSakalauskaite <giedresakalauskaitee@gmail.com>
2023-09-06 14:12:59 +0300

    Add Base class with Testlistener

--- src/test/java/lt/giedre/pom/test/BaseTest.java
+++ src/test/java/lt/giedre/pom/test/BaseTest.java
@@ -1,15 +1,18 @@
 package lt.giedre.pom.test;
 
 import lt.giedre.pom.page.Common;
+import lt.giedre.pom.utilities.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class BaseTest {
     @BeforeMethod
     public abstract void setUp();
 
     @AfterMethod
-    public static void tearDown(){
+    public static void tearDown() {
         Common.quitDriver();
     }
 
